9f5b6371ac541ff1631c90680d03ce61344db391
Vladyslav Boichenko <jiihia1@gmail.com>
2021-09-14 11:39:05 +0300

    redirects upd

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/governance/voting-token",
+        destination: "/governance/token/",
+        statusCode: 301,
+      },
+      {
+        source: "/governance/voting-dashboard",
+        destination: "/governance/dashboard/",
+        statusCode: 301,
+      },
+    ];
+  },
+};

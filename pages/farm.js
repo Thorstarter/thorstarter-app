6a8e5bc47eae16af66c1afed27cb82fa56972956
Frederic Tulsidin <frederic@tulsidin.ca>
2021-11-30 09:50:14 -0500

    publish vaults

--- pages/farm.js
+++ pages/farm.js
@@ -274,18 +274,16 @@ export default function Farm() {
         ))}
       </div>
 
-      {state.networkId === 3 ? (
-        <div className="page-section">
-          <h1 className="title">Vaults</h1>
-          <div className="vaults-grid">
-            {vaults
-              .filter((i) => i.networkId === state.networkId)
-              .map((v) => (
-                <Vault vault={v} key={v.address} />
-              ))}
-          </div>
+      <div className="page-section">
+        <h1 className="title">Vaults</h1>
+        <div className="vaults-grid">
+          {vaults
+            .filter((i) => i.networkId === state.networkId)
+            .map((v) => (
+              <Vault vault={v} key={v.address} />
+            ))}
         </div>
-      ) : null}
+      </div>
     </Layout>
   );
 }

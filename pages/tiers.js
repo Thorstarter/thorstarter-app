7a5aadc7b8a83773ca897e190638c0076765b71a
kiasaki <kiasaki0000@gmail.com>
2022-04-22 12:06:00 +0000

    tiers: add link to mint dao page

--- pages/tiers.js
+++ pages/tiers.js
@@ -367,7 +367,7 @@ export default function Tiers() {
                 <td></td>
               </tr>
               <tr>
-                <td>MintDAO NFT</td>
+                <td><a href="https://app.mintdao.io/collections/thorstarter-shields" target="_blank">MintDAO NFT</a></td>
                 <td className="tac" style={{ width: 110 }}>
                   N/A
                 </td>

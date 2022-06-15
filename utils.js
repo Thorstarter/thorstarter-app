1dbc0faad7814147f2dc609c9de1a05df82a4afe
kiasaki <kiasaki0000@gmail.com>
2022-06-15 11:34:23 -0400

    fix terra gas estimates

--- utils.js
+++ utils.js
@@ -410,8 +410,8 @@ export async function runTransactionTerra(params, setLoading, setError) {
     setError("");
     setLoading(t("waitingForConfirmation"));
     params.gasPrices = new Coins([new Coin("uusd", state.gasPriceUusd)]);
-    //console.log('gas', state.gasPriceUusd)
-    //params.fee = new Fee(750000, { uusd: '2000000' });
+    console.log("gas", state.gasPriceUusd);
+    params.fee = new Fee(750000, { uusd: "2000000" });
     const result = await state.wc.post(params);
     console.log("result", result);
     if (isTxError(result)) {
